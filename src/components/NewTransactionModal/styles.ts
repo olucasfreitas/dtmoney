b919c0f29d1864204a9eb8a3138da1e60d553ff4
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-07-07 18:26:49 -0300

    configurando cor ativa do botão

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from "polished";
+import { darken, transparentize } from "polished";
 
 export const Container = styled.div`
   h2 {
@@ -53,13 +53,22 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
   isActive: boolean;
+  activeColor: "green" | "red";
 }
+
+const colors = {
+  green: "#33Cc95",
+  red: "#E62E4D",
+};
 
 export const RadioBox = styled.button<RadioBoxProps>`
   height: 4rem;
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
-  background: ${(props) => (props.isActive ? "#eee" : "transparent")};
+  background: ${(props) =>
+    props.isActive
+      ? transparentize(0.9, colors[props.activeColor])
+      : "transparent"};
   display: flex;
   align-items: center;
   justify-content: center;
