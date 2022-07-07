dcad80393bb0ec046c6db111c69b8b463530b0e8
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-07-07 15:08:18 -0300

    configurando fonte

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -32,12 +32,22 @@ export const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
   }
 
+
+  body, input, button, textarea {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
   button {
     cursor: pointer;
   }
 
   [disabled] {
     opacity: 0.6;
-    cursor: not-alloweds;
+    cursor: not-allowed;
   }
 `;
